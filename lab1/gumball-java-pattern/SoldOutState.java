251f7246ec0238dde2f900f6a586f5fe8d6b5773
David Montes <dpmontes@ucdavis.edu>
2018-09-07 15:24:27 -0700

    updated SoldOutState

--- lab1/gumball-java-pattern/SoldOutState.java
+++ lab1/gumball-java-pattern/SoldOutState.java
@@ -8,19 +8,59 @@ public class SoldOutState implements State {
     }
  
 	public void insertCoin( int value, GumballMachine.Type type ) {
-		System.out.println("You can't insert a quarter, the machine is sold out");
+		switch(type)
+		{
+			case OneQuarter:
+			case TwoQuarters:
+				System.out.println("You can't insert a quarter, the machine is sold out");
+				break;
+			case AllCoins:
+				System.out.println("You can't insert a coin, the machine is sold out");
+				break;
+			default:
+				break;
+		}
 	}
  
 	public void ejectMoney( int money, GumballMachine.Type type ) {
-		System.out.println("You can't eject, you haven't inserted a quarter yet");
+		switch(type)
+		{
+			case OneQuarter:
+			case TwoQuarters:
+				System.out.println("You can't eject, you haven't inserted a quarter yet");
+				break;
+			case AllCoins:
+				System.out.println("You can't eject, you haven't inserted a coin yet");
+				break;
+			default:
+				break;
+		}
 	}
  
 	public void turnCrank( GumballMachine.Type type ) {
-		System.out.println("You turned, but there are no gumballs");
+		switch(type)
+		{
+			case OneQuarter:
+			case TwoQuarters:
+			case AllCoins:
+				System.out.println("You turned, but there are no gumballs");
+				break;
+			default:
+				break;
+		}
 	}
  
 	public void dispense( GumballMachine.Type type ) {
-		System.out.println("No gumball dispensed");
+		switch(type)
+		{
+			case OneQuarter:
+			case TwoQuarters:
+			case AllCoins:
+				System.out.println("No gumball dispensed");
+				break;
+			default:
+				break;
+		}
 	}
  
 	public String toString() {
